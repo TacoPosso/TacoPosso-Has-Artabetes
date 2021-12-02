3ba3c5ee014e30649af880f3ca1ce848c2afb2d7
EMRobinson <69981633+EMRobinson@users.noreply.github.com>
2021-12-02 02:36:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Currency;
+
+import java.util.Scanner;
+
+class Main {  
+  
+  
+  public static void main(String[] args) {
+  
+  double bagCook = 40;
+  double servCook = 10;
+  double caliCook = 300;
+
+  Scanner reader = new Scanner(System.in);
+ 
+ System.out.println("How many cookies did you eat?");
+double n = reader.nextDouble();
+reader.close();
+
+//converter object
+//method or a function
+//() pass argument 
+
+System.out.println(""(stateTax));
+System.out.println("County tax is " + nF.format(countyTax));
+System.out.println("total tax is " + nF.format(totalTax));
+System.out.println("The actual price of the thing due to the fuckery of capitalism is " + nF.format(totalPrice));
+
+}}
